42c7867205a4236c4ad99252453f7468a817582f
Peter Rauscher <peterrauscher@protonmail.com>
2024-02-18 09:05:57 -0500

    adding callbacks for stockx api oauth

---
+++ src/api/routes/auth.py
@@ -0,0 +1,31 @@
+from http.client import HTTPException
+import os
+from fastapi import APIRouter
+import requests
+
+STOCKX_CLIENT_ID = os.environ.get("SOLESEARCH_STOCKX_CLIENT_ID", None)
+STOCKX_CLIENT_SECRET = os.environ.get("SOLESEARCH_STOCKX_CLIENT_SECRET", None)
+STOCKX_API_KEY = os.environ.get("SOLESEARCH_STOCKX_API_KEY", None)
+
+router = APIRouter(
+    prefix="/auth",
+)
+
+@router.get("/stockx/code")
+async def get_stockx_auth(state: str = None, code: str = None):
+    if state != 'pRbqDm7XTFx27WZyQW78urwKWmZJW3go':
+        raise HTTPException(status_code=401, detail='Bad secret. Nice try, buster.')
+    if code is None:
+        raise HTTPException(status_code=400, detail='No code provided')
+    response = requests.post(url='https://accounts.stockx.com/oauth/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        "client_id": STOCKX_CLIENT_ID,
+        "client_secret": STOCKX_CLIENT_SECRET,
+        "redirect_uri": "http://localhost:8000/auth/stockx/token",
+    })
+    return {"Error": "Not implemented yet"}
+
+@router.get("/stockx/token")
+async def get_stockx_token():
+    return {"Error": "Not implemented yet"}
