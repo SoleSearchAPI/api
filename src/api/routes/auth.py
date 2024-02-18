83dee5af228173d7fd5725a1d3b530faf1da967f
Peter Rauscher <peter@peterrauscher.com>
2024-02-18 10:44:32 -0500

    added SSL required for oauth callbacks

--- src/api/routes/auth.py
+++ src/api/routes/auth.py
@@ -1,7 +1,9 @@
-from http.client import HTTPException
+import logging
 import os
-from fastapi import APIRouter
+from http.client import HTTPException
+
 import requests
+from fastapi import APIRouter
 
 STOCKX_CLIENT_ID = os.environ.get("SOLESEARCH_STOCKX_CLIENT_ID", None)
 STOCKX_CLIENT_SECRET = os.environ.get("SOLESEARCH_STOCKX_CLIENT_SECRET", None)
@@ -11,21 +13,29 @@ router = APIRouter(
     prefix="/auth",
 )
 
+
 @router.get("/stockx/code")
 async def get_stockx_auth(state: str = None, code: str = None):
-    if state != 'pRbqDm7XTFx27WZyQW78urwKWmZJW3go':
-        raise HTTPException(status_code=401, detail='Bad secret. Nice try, buster.')
+    if state != "pRbqDm7XTFx27WZyQW78urwKWmZJW3go":
+        raise HTTPException(status_code=401, detail="Bad secret. Nice try, buster.")
     if code is None:
-        raise HTTPException(status_code=400, detail='No code provided')
-    response = requests.post(url='https://accounts.stockx.com/oauth/token', data={
-        'grant_type': 'authorization_code',
-        'code': code,
-        "client_id": STOCKX_CLIENT_ID,
-        "client_secret": STOCKX_CLIENT_SECRET,
-        "redirect_uri": "http://localhost:8000/auth/stockx/token",
-    })
-    return {"Error": "Not implemented yet"}
+        raise HTTPException(status_code=400, detail="No code provided")
+    response = requests.post(
+        url="https://accounts.stockx.com/oauth/token",
+        data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "client_id": STOCKX_CLIENT_ID,
+            "client_secret": STOCKX_CLIENT_SECRET,
+            "redirect_uri": "https://localhost:8000/auth/stockx/token",
+        },
+    )
+    response_json = response.json()
+    logging.info(response_json)
+    return response_json
+
 
 @router.get("/stockx/token")
-async def get_stockx_token():
-    return {"Error": "Not implemented yet"}
+async def get_stockx_token(authorization_code: str = None):
+    logging.info(authorization_code)
+    return {"Successs": "Check the logs for authorization code"}
