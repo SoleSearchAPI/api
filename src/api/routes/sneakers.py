d6239d49346a40423abeea627f692b164424e3e4
Peter Rauscher <peterrauscher@protonmail.com>
2024-02-26 01:20:58 -0500

    fully switched to beanie

--- src/api/routes/sneakers.py
+++ src/api/routes/sneakers.py
@@ -1,12 +1,12 @@
+from datetime import UTC, datetime
 from typing import Annotated
 
+from core.models.details import Audience
+from core.models.shoes import Sneaker
 from fastapi import APIRouter, HTTPException, Query
 
 from api.data.instance import DEFAULT_LIMIT, DEFAULT_OFFSET
 from api.data.models import SortKey, SortOrder
-from api.data.queries import find_sneakers
-from core.models.details import Audience
-from core.models.shoes import Sneaker
 
 router = APIRouter(
     prefix="/sneakers",
@@ -16,30 +16,49 @@ router = APIRouter(
 @router.get("/")
 async def get_sneakers(
     brand: str | None = None,
-    sku: str | None = None,
     name: str | None = None,
-    colorway: str | None = None,
     audience: Audience | None = None,
     releaseDate: str | None = None,
     released: bool | None = None,
     sort: SortKey = SortKey.RELEASE_DATE,
-    sortOrder: SortOrder = SortOrder.DESCENDING,
+    order: SortOrder = SortOrder.DESCENDING,
     offset: Annotated[int, Query(gte=DEFAULT_OFFSET)] = DEFAULT_OFFSET,
     limit: Annotated[int, Query(gte=1, lte=100)] = DEFAULT_LIMIT,
 ):
-    return await find_sneakers(
-        brand=brand,
-        sku=sku,
-        name=name,
-        colorway=colorway,
-        audience=audience,
-        release_date=releaseDate,
-        released=released,
-        sort_by=sort,
-        sort_order=sortOrder,
-        offset=offset,
-        limit=limit,
+    query = Sneaker.find()
+    if brand:
+        query = query.find(Sneaker.brand == brand)
+    if name:
+        query = query.find(Sneaker.name == name)
+    if audience:
+        query = query.find(Sneaker.audience == audience)
+    if released is not None:
+        now = datetime.now(UTC)
+        if released:
+            query = query.find(Sneaker.releaseDate <= now)
+        else:
+            query = query.find(Sneaker.releaseDate > now)
+    elif releaseDate:
+        if ":" in releaseDate:
+            inequality_operator, date_str = releaseDate.split(":")
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+            if inequality_operator == "lt":
+                query = query.find(Sneaker.releaseDate < date_obj)
+            elif inequality_operator == "lte":
+                query = query.find(Sneaker.releaseDate <= date_obj)
+            elif inequality_operator == "gt":
+                query = query.find(Sneaker.releaseDate > date_obj)
+            elif inequality_operator == "gte":
+                query = query.find(Sneaker.releaseDate >= date_obj)
+        else:
+            date_obj = datetime.strptime(releaseDate, "%Y-%m-%d")
+            query = query.find(Sneaker.releaseDate == date_obj)
+
+    order = "+" if order == SortOrder.ASCENDING else "-"
+    sneakers_list = (
+        await query.sort(f"{order}{sort.value}").skip(offset).limit(limit).to_list()
     )
+    return sneakers_list
 
 
 @router.get("/{product_id}")
